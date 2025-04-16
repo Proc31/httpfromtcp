bba5db2d1a04f79a2c028aae818592a2b59ba7c7
Matt Proctor <gkproc@gmail.com>
2025-04-16 17:10:50 +0100

    days work

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,55 @@
 package request
+
+import (
+	"errors"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func catch(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	b, err := io.ReadAll(reader)
+	catch(err)
+	line, err := parseRequestLine(b)
+
+	request := Request{
+		RequestLine: line,
+	}
+
+	return &request, err
+}
+
+func parseRequestLine(b []byte) (RequestLine, error) {
+	str := string(b)
+	parts := strings.Fields(str)
+	ver := strings.TrimPrefix(parts[2], "HTTP/")
+
+	err := errors.New(nil)
+
+	if parts[1] != strings.ToUpper(parts[1]) {
+		err = errors.New("method type bad")
+	}
+
+	requestLine := RequestLine{
+		HttpVersion:   ver,
+		RequestTarget: parts[1],
+		Method:        parts[0],
+	}
+
+	return requestLine, err
+}
